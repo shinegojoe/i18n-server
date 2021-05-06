f1d38abbf94d21c96ea732f6712473688e5c70ab
taka <shinegojoe@gmail.com>
2021-05-06 20:30:26 +0800

    test

--- test/index.ts
+++ test/index.ts
@@ -10,23 +10,12 @@ import { LanguageTester } from './apiTest.test/language.test'
 
 const dbPath: any = process.env['SQLITE_PATH']
 console.log('dbPath', dbPath)
+
+
 describe('test start', ()=> {
-  // require('./apiTest.test/project.test')
   const projectTester = new ProjectTester('project', 'name')
   projectTester.run()
-
   const langTester = new LanguageTester('language', 'name')
   langTester.run()
-  // describe("mongo test", ()=> {
-  //   require('./dbTest.test/mongo.test')
-  // })
-
-  // describe("api test", ()=> {
-  //   // require('./apiTest.test/stockList.test')
-  //   require('./apiTest.test/stockInfo.test')
-  // })
-
-  // describe('model test', ()=> {
-  //   require('./modelTest.test/rbac.test/user.test')
-  // })
 })
+
