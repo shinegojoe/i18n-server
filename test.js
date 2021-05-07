18ec6d9e0ad86afedfc502a37568819c1d6392bb
taka <taka@huangguanghongdeMac-mini.local>
2021-05-07 09:35:56 +0800

    deploy test

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log('test')
+import axios from 'axios'
+
+const url = 'http://203.204.160.248:5001'
+const xx = axios.get(url)
+xx.then(()=> {
+  console.log('test done')
+}).catch((e)=> {
+  console.log('e', e)
+})
+
