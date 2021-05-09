c014d7ce43b2f35538f164dd9b9eb2ff191c5c8d
taka <shinegojoe@gmail.com>
2021-05-09 19:56:19 +0800

    route update

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,10 +6,6 @@ import row from './row.route'
 import projectLang from './projectLang.route'
 import deploy from './deploy.ruote'
 
-const fs = require('fs')
-const { exec } = require("child_process")
-
-
 
 const router = Router()
 router.use('/', project)
@@ -19,63 +15,10 @@ router.use('/', row)
 router.use('/', projectLang)
 router.use('/', deploy)
 
-
-
-router.get('/ttt', (req, res, next)=> {
-    console.log('get ttt')
-    res.send('one qk rock')
+router.get('/test', (req, res, next)=> {
+    console.log('get test')
+    res.send('one qq rock')
 })
-
-
-// const run = () => {
-//     exec("tar xvf dist.tar", (error: any, stdout: any, stderr: any) => {
-//         if (error) {
-//             console.log(`error: ${error.message}`);
-//             return;
-//         }
-//         // if (stderr) {
-//         //     console.log(`stderr: ${stderr}`);
-//         //     return;
-//         // }
-//         console.log(`run: ${stdout}`);
-//      });
-// }
-
-// const run2 = () => {
-//     exec("npm run restart", (error: any, stdout: any, stderr: any) => {
-//         if (error) {
-//             console.log(`error: ${error.message}`);
-//             return;
-//         }
-//         // if (stderr) {
-//         //     console.log(`stderr: ${stderr}`);
-//         //     return;
-//         // }
-//         console.log(`run2: ${stdout}`);
-//      });
-// }
-
-// router.post('/ttt', (req, res, next)=> {
-//   console.log(req.body)
-//   // const data = req.body
-//   // // const data = new Uint8Array(Buffer.from('Hello Node.js'));
-//   // fs.writeFile('qqq.tar', data.data, (err: Error) => {
-//   //   if (err) throw err;
-//   //   console.log('The file has been saved!');
-//   // });
-//   const b = Buffer.from(req.body.data)
-//   fs.writeFile('dist.tar', b, (err: Error)=> {
-//     if(err) {
-//       console.log(err)
-//     }
-//     console.log('ok')
-//     run()
-//     run2()
-//   })
-  
-//   res.send('ok')
-// })
-
 
 export default router
 
