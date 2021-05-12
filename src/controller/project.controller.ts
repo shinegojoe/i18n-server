4e891b2f7a46352e52ebbe2003dbfcb3f2b87f58
taka <shinegojoe@gmail.com>
2021-05-12 21:18:28 +0800

    add log

--- src/controller/project.controller.ts
+++ src/controller/project.controller.ts
@@ -5,6 +5,10 @@ import { BaseController } from './base.controller'
 const dbPath: any = process.env['SQLITE_PATH']
 const sqliteHelper = new SqliteHelper(dbPath)
 
+console.log('dbPath', dbPath)
+console.log('sqliteHelper', sqliteHelper)
+
+
 const model = new ProjectModel(sqliteHelper, cfg)
 
 class ProjectController extends BaseController {
